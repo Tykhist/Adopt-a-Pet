57635408b37b2c88492678dab808301ff85d5a9c
William Smith <mr.williamjdsmith@gmail.com>
2022-07-20 09:50:20 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from helper import pets
 
 app = Flask(__name__)
 
@@ -8,9 +9,9 @@ def index():
   <h1>Adopt a Pet!</h1>
   <p>Browse through the links below to find your new furry friend:</p>
   <ul>
-    <li><a href='/animals/Dogs'>Dogs</a></li>
-    <li><a href='/animals/Cats'>Cats</a></li>
-    <li><a href='/animals/Rabbits'>Rabbits</a></li>
+    <li><a href='/animals/dogs'>dogs</a></li>
+    <li><a href='/animals/cats'>cats</a></li>
+    <li><a href='/animals/rabbits'>rabbits</a></li>
   </ul>
   """
 
@@ -19,5 +20,15 @@ def animals(pet_type):
   html = f"""
   <h1>List of {pet_type}</h1>
   <a href='/'>Return to Home</a>
-  """
+  """ 
+  html += "<ul>"
+  for i in pets[pet_type]:
+    html += f"<li>{i['name']}</li>"
+  html += "<ul>"
   return html
+
+@app.route('/animals/<pet_type>/<int:pet_id>')
+def pet(pet_type, pet_id):
+  pet = pets[pet_type][pet_id]
+
+  return f"<h1>{pet['name']}"
